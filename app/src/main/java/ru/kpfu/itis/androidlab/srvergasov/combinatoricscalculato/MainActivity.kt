a788130b9027abc64216c0de3828ec4664387f5f
Shamil Vergasov <SRVergasov@stud.kpfu.ru>
2022-10-02 20:18:20 +0300

    Update MainActivity.kt

--- app/src/main/java/ru/kpfu/itis/androidlab/srvergasov/combinatoricscalculato/MainActivity.kt
+++ app/src/main/java/ru/kpfu/itis/androidlab/srvergasov/combinatoricscalculato/MainActivity.kt
@@ -1,11 +1,29 @@
 package ru.kpfu.itis.androidlab.srvergasov.combinatoricscalculato
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.kpfu.itis.androidlab.srvergasov.combinatoricscalculato.calculator.Processor
+import ru.kpfu.itis.androidlab.srvergasov.combinatoricscalculato.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var processor: Processor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun initProcessor() {
+        processor = Processor(
+            getString(R.string.fnc_name_perm),
+            getString(R.string.fnc_name_permRep),
+            getString(R.string.fnc_name_plac),
+            getString(R.string.fnc_name_placRep),
+            getString(R.string.fnc_name_comb),
+            getString(R.string.fnc_name_combRep),
+        )
     }
 }
