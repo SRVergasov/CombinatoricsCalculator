bd2a3c8ab1ebfd9532f133ac8d43643f3c429243
Shamil Vergasov <SRVergasov@stud.kpfu.ru>
2022-10-28 02:26:17 +0300

    upgrade layout/layout logic

--- app/src/main/java/ru/kpfu/itis/androidlab/srvergasov/combinatoricscalculator/MainActivity.kt
+++ app/src/main/java/ru/kpfu/itis/androidlab/srvergasov/combinatoricscalculator/MainActivity.kt
@@ -1,7 +1,10 @@
 package ru.kpfu.itis.androidlab.srvergasov.combinatoricscalculator
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import ru.kpfu.itis.androidlab.srvergasov.combinatoricscalculator.calculator.CalculatorProcessor
 import ru.kpfu.itis.androidlab.srvergasov.combinatoricscalculator.databinding.ActivityMainBinding
 import ru.kpfu.itis.androidlab.srvergasov.combinatoricscalculator.spinner.SpinnerProcessor
@@ -19,7 +22,38 @@ class MainActivity : AppCompatActivity() {
         initSpinner()
 
         with(binding) {
+            spinnerFncName.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                override fun onNothingSelected(parent: AdapterView<*>?) {
 
+                }
+
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    setEtVisibility(spinnerFncName.selectedItem.toString())
+                }
+
+            }
+        }
+    }
+
+    private fun setEtVisibility(funcName: String) {
+        with(binding) {
+            when (funcName) {
+                getString(R.string.fnc_name_permRep) -> {
+                    et1.visibility = View.GONE
+                    et2.visibility = View.GONE
+                    et3.visibility = View.VISIBLE
+                }
+                getString(R.string.fnc_name_perm) -> {
+                    et1.visibility = View.VISIBLE
+                    et2.visibility = View.GONE
+                    et3.visibility = View.GONE
+                }
+                else -> {
+                    et1.visibility = View.VISIBLE
+                    et2.visibility = View.VISIBLE
+                    et3.visibility = View.GONE
+                }
+            }
         }
     }
 
